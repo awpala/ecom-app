205ea324619c7d33cfcfd6097a36b7fa7d459331
awpala <apala7@gatech.edu>
2023-02-12 01:29:18 +0000

    flatten errors to single array in JSON response

---
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,13 @@
+using System.Net;
+
+namespace API.Errors
+{
+  public class ApiValidationErrorResponse : ApiResponse
+  {
+    public ApiValidationErrorResponse() : base((int)HttpStatusCode.BadRequest)
+    {
+    }
+
+    public IEnumerable<string> Errors { get; set; }
+  }
+}
