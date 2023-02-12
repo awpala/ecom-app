af0627f54959485fe7106818129df743f9f80afa
awpala <apala7@gatech.edu>
2023-02-12 00:06:11 +0000

    refactor: implement specification pattern for products controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -26,14 +27,16 @@ namespace API.Controllers
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
     {
-      var products = await _productsRepo.ListAllAsync();
+      var spec = new ProductsWithTypesAndBrandsSpecification();
+      var products = await _productsRepo.ListAsync(spec);
       return Ok(products);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      var product = await _productsRepo.GetByIdAsync(id);
+      var spec = new ProductsWithTypesAndBrandsSpecification(id);
+      var product = await _productsRepo.GetEntityWithSpec(spec);
       return Ok(product);
     }
 
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -7,6 +8,7 @@ namespace Infrastructure.Data
   public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
   {
     private readonly StoreContext _context;
+
     public GenericRepository(StoreContext context)
     {
       _context = context;
@@ -22,6 +24,23 @@ namespace Infrastructure.Data
     {
       var entities = await _context.Set<T>().ToListAsync();
       return entities;
+    }
+
+    public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+    {
+      var entity = await ApplySpecification(spec).FirstOrDefaultAsync();
+      return entity;
+    }
+
+    public async  Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+    {
+      var entities = await ApplySpecification(spec).ToListAsync();
+      return entities;
+    }
+
+    private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+    {
+      return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
     }
   }
 }
