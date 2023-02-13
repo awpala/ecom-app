e2930acd6fb573ea340c3a69d79cf5a7c9c394d5
awpala <apala7@gatech.edu>
2023-02-13 06:05:07 +0000

    fix(client): reset `pageIndex` on filter and/or on search

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -61,11 +61,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageIndex = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageIndex = 1;
     this.getProducts();
   }
 
@@ -83,6 +85,7 @@ export class ShopComponent implements OnInit {
 
   onSearch() {
     this.shopParams.search = this.searchTerm?.nativeElement.value;
+    this.shopParams.pageIndex = 1;
     this.getProducts();
   }
 
