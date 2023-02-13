98e6596e836a56c25382322f13b1e5a044eb8b49
awpala <apala7@gatech.edu>
2023-02-13 05:21:40 +0000

    refactor(client): extract reusable component `PagingHeaderComponent`

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagingHeaderComponent,
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(), // N.B. method `forRoot()` loads `PaginationModule` as a singleton, to be used throughout the app (i.e., only one instance is required)
   ],
   exports: [
     PaginationModule,
+    PagingHeaderComponent,
   ],
 })
 export class SharedModule {}
