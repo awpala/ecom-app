c86b0bf80cfdb41b91093fcc782ddf97828b4745
awpala <apala7@gatech.edu>
2023-02-18 17:32:09 +0000

    client: initialize routing and add corresponding components

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
   ],
   exports: [
     NavBarComponent,
