b82cbf7c1fa6481e926b1cdc088ed881bd165420
awpala <apala7@gatech.edu>
2023-02-18 17:52:09 +0000

    client: add routing for component `ProductDetailsComponent`

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -27,6 +27,10 @@ export class ShopService {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', { params });
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + `products/${id}`);
+  }
+
   getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
   }
@@ -34,5 +38,4 @@ export class ShopService {
   getTypes() {
     return this.http.get<Type[]>(this.baseUrl + 'products/types');
   }
-
 }
