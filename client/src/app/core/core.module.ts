661aea33c794ab0ffb32c8988318db98d6e67b82
awpala <apala7@gatech.edu>
2023-02-19 20:37:47 +0000

    client: add page loading logic and corresponding spinner `timer`

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -23,10 +24,12 @@ import { BreadcrumbModule } from 'xng-breadcrumb';
       preventDuplicates: true,
     }),
     BreadcrumbModule,
+    NgxSpinnerModule,
   ],
   exports: [
     NavBarComponent,
     SectionHeaderComponent,
+    NgxSpinnerModule,
   ]
 })
 export class CoreModule { }
