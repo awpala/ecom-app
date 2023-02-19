2d7559e50af5f6c3074f02de9e2b4406ce8b1b20
awpala <apala7@gatech.edu>
2023-02-19 20:54:44 +0000

    client: update Home view

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './paging-header/paging-header.component';
 import { PagerComponent } from './pager/pager.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
@@ -12,11 +13,13 @@ import { PagerComponent } from './pager/pager.component';
   imports: [
     CommonModule,
     PaginationModule.forRoot(), // N.B. method `forRoot()` loads `PaginationModule` as a singleton, to be used throughout the app (i.e., only one instance is required)
+    CarouselModule.forRoot(),
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
+    CarouselModule,
   ],
 })
 export class SharedModule {}
