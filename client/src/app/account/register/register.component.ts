7aa6c11ae3a21ac7338d751b031fe8a6958ce132
awpala <apala7@gatech.edu>
2023-02-27 20:06:34 +0000

    client: implement component `RegisterComponent` along with appropriate form input validations

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router,
+  ) {}
 
+  // reference: https://regexlib.com/REDetails.aspx?regexp_id=1111
+  complexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$";
+
+  registerForm = this.fb.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(this.complexPassword)]],
+  });
+
+  onSubmit() {
+    this.accountService
+      .register(this.registerForm.value)
+      .subscribe({
+        next: () => this.router.navigateByUrl('/shop'),
+      });
+  }
 }
